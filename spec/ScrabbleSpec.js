8a2c0eefcda1fddd525b0a0a5b9ef32311ea4f74
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-02 20:17:06 +0100

    Fifth test pass, add hash table

--- spec/ScrabbleSpec.js
+++ spec/ScrabbleSpec.js
@@ -14,16 +14,24 @@ describe('Scrabble', function() {
 
   it('returns 1 when the input is "a"', function() {
     string = 'a';
+
     expect(scrabble.score()).toEqual(1);
   })
 
   it('returns 4 when the input is "f"', function() {
     string = 'f';
+
     expect(scrabble.score()).toEqual(4);
   })
 
   it('returns 0 when input is null', function() {
     string = null;
     expect(scrabble.score()).toEqual(0);
+  })
+
+  it('returns 2 when input is "aa"', function() {
+    string = 'aa';
+    console.log(scrabble.score())
+    expect(scrabble.score()).toEqual(2);
   })
 })
--- src/Scrabble.js
+++ src/Scrabble.js
@@ -1,14 +1,31 @@
 class Scrabble {
   constructor(string) {
     this.string = string;
+    //create a hash that represents the scores for letters
+    this.table = {
+      'AEIOULNRST': 1,
+      'DG': 2,
+      'BCMP': 3,
+      'FHVMY': 4,
+      'K': 5,
+      'JX': 8,
+      'QZ': 10
+    }
   }
   score() {
     if (string === null || string.length === 0 ) {
       return 0;
-    } else if (string === 'a') {
-      return 1;
-    } else if (string === 'f') {
-      return 4;
+    } else {
+      let array = string.split('');
+      let total = 0;
+      array.forEach((arrayItem, i) => {
+        Object.keys(this.table).forEach((row, i) => {
+          if (row.includes(arrayItem.toUpperCase())) {
+            total = total + this.table[row];
+          }
+        });
+      });
+      return total;
     }
   }
 }
